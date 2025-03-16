7d76b386b3e963785c33f7177699a40b4eb75ad7
Carlos Eduardo Lima de Oliveira <161634282+kaduolliver@users.noreply.github.com>
2025-03-15 22:00:36 -0300

    Add files via upload

---
+++ with_poo/src/application12/Bank.java
@@ -0,0 +1,17 @@
+package application12;
+
+public class Bank {
+
+    private final int accNumber;
+    private String titleName;
+    private double balance;
+
+    public Bank(int accNumber, String tittleName, double initialBalance) {
+        this.accNumber = accNumber;
+        this.titleName = titleName;
+        this.balance = initialBalance;
+    }
+
+    
+
+}
